f72c3bf9ef6fb75d0afac908fed2dab44b4c9905
MSI-I7 <23101084@ue.edu.pe>
2026-05-30 11:03:19 -0500

    Feat: Implementacion de la Pantalla 0 Menu Principal y estructura de navegacion

--- app/src/main/java/com/example/pc01capcha23100750elias23101084/MainActivity.kt
+++ app/src/main/java/com/example/pc01capcha23100750elias23101084/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.pc01capcha23100750elias23101084.navigation.NavGraph
 import com.example.pc01capcha23100750elias23101084.ui.theme.PC01CAPCHA23100750ELIAS23101084Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PC01CAPCHA23100750ELIAS23101084Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PC01CAPCHA23100750ELIAS23101084Theme {
-        Greeting("Android")
     }
 }
